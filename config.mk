b8f79f0ed0db553745798cd4fb43329ba5c61cef
Matthias-Christian Ott <ott@enolink.de>
2008-06-08 21:36:58 +0200

    remove xinerama flags
    The xinerama related variables in config.mk are a relict of
    dwm and therefore should be removed.

--- config.mk
+++ config.mk
@@ -10,16 +10,12 @@ MANPREFIX = ${PREFIX}/share/man
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-# Xinerama, comment if you don't want it
-XINERAMALIBS = -L${X11LIB} -lXinerama
-XINERAMAFLAGS = -DXINERAMA
-
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 ${XINERAMALIBS}
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11
 
 # flags
-CPPFLAGS = -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CPPFLAGS = -DVERSION=\"${VERSION}\"
 CFLAGS = -Os ${INCS} ${CPPFLAGS}
 LDFLAGS = -s ${LIBS}
 #CFLAGS = -g -std=c99 -pedantic -Wall -O2 ${INCS} ${CPPFLAGS}
