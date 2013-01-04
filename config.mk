4682cc9596a5c82325882bdf1313728de95467d2
Christoph Lohmann <20h@r-36.net>
2013-01-04 20:52:22 +0100

    Making the fontconfig and freetype paths portable in config.mk.

--- config.mk
+++ config.mk
@@ -11,8 +11,12 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC} -I/usr/include/freetype2
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lutil -lXext -lXft -lfontconfig
+INCS = -I. -I/usr/include -I${X11INC} \
+       $(shell pkg-config --cflags fontconfig) \
+       $(shell pkg-config --cflags freetype2)
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 -lutil -lXext -lXft \
+       $(shell pkg-config --libs fontconfig)  \
+       $(shell pkg-config --libs freetype2)
 
 # flags
 CPPFLAGS = -DVERSION=\"${VERSION}\"
