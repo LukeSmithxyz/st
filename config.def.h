2cf4f366d615a4e5ed199f90e6cdb7741606a38e
Christoph Lohmann <20h@r-36.net>
2013-06-01 14:39:13 +0200

    Bumping up the xfps so mouse motion won't reap the CPU.

--- config.def.h
+++ config.def.h
@@ -24,7 +24,7 @@ static unsigned int tripleclicktimeout = 600;
 static bool allowaltscreen = true;
 
 /* frames per second st should at maximum draw to the screen */
-static unsigned int xfps = 60;
+static unsigned int xfps = 120;
 static unsigned int actionfps = 30;
 
 /*
