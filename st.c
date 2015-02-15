c5f1d74fd83319d84887c7ff6769f3d42c3d1a0d
Nils Reuße <ml@hxgn.net>
2015-02-15 17:46:15 +0100

    Update year in usage()

--- st.c
+++ st.c
@@ -3938,7 +3938,7 @@ run(void) {
 
 void
 usage(void) {
-	die("%s " VERSION " (c) 2010-2014 st engineers\n" \
+	die("%s " VERSION " (c) 2010-2015 st engineers\n" \
 	"usage: st [-a] [-v] [-c class] [-f font] [-g geometry] [-o file]\n"
 	"          [-i] [-t title] [-w windowid] [-e command ...]\n", argv0);
 }
