28259f5750f0dc7f52bbaf8b746ec3dc576a58ee
Christoph Lohmann <20h@r-36.net>
2015-03-10 21:58:32 +0100

    St now does only set PRIMARY on selection.
    http://standards.freedesktop.org/clipboards-spec/clipboards-latest.txt

--- st.c
+++ st.c
@@ -1079,16 +1079,9 @@ selrequest(XEvent *e) {
 
 void
 xsetsel(char *str) {
-	/* register the selection for both the clipboard and the primary */
-	Atom clipboard;
-
 	free(sel.clip);
 	sel.clip = str;
-
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, CurrentTime);
-
-	clipboard = XInternAtom(xw.dpy, "CLIPBOARD", 0);
-	XSetSelectionOwner(xw.dpy, clipboard, xw.win, CurrentTime);
 }
 
 void
