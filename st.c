c27c731b9f77c1b1c99f3bde737be53996809fcb
noname@inventati.org <noname@inventati.org>
2015-04-18 18:46:17 +0200

    Monotonic clock cannot jump backwards.
    The check was introduced back when st used gettimeofday.
    The condition is also modified to increment the accuaracy of the
    calculation.

--- st.c
+++ st.c
@@ -4003,8 +4003,7 @@ run(void) {
 			dodraw = 1;
 		}
 		deltatime = TIMEDIFF(now, last);
-		if(deltatime > (xev? (1000/xfps) : (1000/actionfps))
-				|| deltatime < 0) {
+		if(deltatime > 1000 / (xev ? xfps : actionfps)) {
 			dodraw = 1;
 			last = now;
 		}
