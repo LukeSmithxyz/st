bf8c71e42937b623f76ced22c9557f87e3d945da
Christoph Lohmann <20h@r-36.net>
2015-09-12 16:43:32 +0200

    The times of bad fonts are over.
    The antialiasing was false due to circumstances that do not exist anymore. We
    need antialiasing on big screens with big fonts. Autohinting too.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=12:antialias=false:autohint=false";
+static char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
