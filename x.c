77c51c5a6b16387f1792e23acbcf2080f790aa25
Anselm R Garbe <anselm@garbe.us>
2017-09-01 09:48:24 +0200

    make clipboard patch obsolete

--- x.c
+++ x.c
@@ -507,6 +507,7 @@ xsetsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear_(NULL);
+	xclipcopy();
 }
 
 void
