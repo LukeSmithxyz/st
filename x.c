b81888ee7d95d9ecb7b1749630b09a96065a8fea
Hiltjo Posthuma <hiltjo@codemadness.org>
2018-03-09 15:36:25 +0100

    xhints: no need to initialize sizeh

--- x.c
+++ x.c
@@ -780,7 +780,7 @@ xhints(void)
 	XClassHint class = {opt_name ? opt_name : termname,
 	                    opt_class ? opt_class : termname};
 	XWMHints wm = {.flags = InputHint, .input = 1};
-	XSizeHints *sizeh = NULL;
+	XSizeHints *sizeh;
 
 	sizeh = XAllocSizeHints();
 
