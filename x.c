732be223ee7ba5486713c63f944699fd6285af96
Hiltjo Posthuma <hiltjo@codemadness.org>
2018-07-17 20:01:54 +0200

    Revert "Fix crash when cursor color is truecolor"
    This reverts commit 5535c1f04c665c05faff2a65d5558246b7748d49.

--- x.c
+++ x.c
@@ -1404,7 +1404,6 @@ void
 xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og)
 {
 	Color drawcol;
-	uint32_t cc;
 
 	/* remove the old cursor */
 	if (selected(ox, oy))
@@ -1420,22 +1419,14 @@ xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og)
 	g.mode &= ATTR_BOLD|ATTR_ITALIC|ATTR_UNDERLINE|ATTR_STRUCK|ATTR_WIDE;
 
 	if (selected(cx, cy)) {
-		cc = g.bg;
+		drawcol = dc.col[g.bg];
 	} else {
 		g.mode |= ATTR_REVERSE;
-		if (g.mode & ATTR_BOLD && BETWEEN(g.fg, 0, 7))
-			cc = g.fg + 8;
-		else
-			cc = g.fg;
-	}
 
-	if (IS_TRUECOL(cc)) {
-		drawcol.color.alpha = 0xffff;
-		drawcol.color.red = TRUERED(cc);
-		drawcol.color.green = TRUEGREEN(cc);
-		drawcol.color.blue = TRUEBLUE(cc);
-	} else {
-		drawcol = dc.col[cc];
+		if (g.mode & ATTR_BOLD && BETWEEN(g.fg, 0, 7))
+			drawcol = dc.col[g.fg + 8];
+		else
+			drawcol = dc.col[g.fg];
 	}
 
 	if (IS_SET(MODE_REVERSE)) {
