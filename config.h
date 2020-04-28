b7b9c54a10a29e35eba914463f2e67630d5af3f4
Luke Smith <luke@lukesmith.xyz>
2020-04-28 19:09:15 -0400

    less opaque to compensate for darker bg

--- config.h
+++ config.h
@@ -91,7 +91,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.92;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
