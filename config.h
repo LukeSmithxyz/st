a09200ee395ce1c938c71dc8c0302eb032cd0604
wbmins <wbminions@gmail.com>
2020-12-16 08:20:30 +0800

    update copy patch

--- config.h
+++ config.h
@@ -154,7 +154,7 @@ unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+ static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
--- x.c
+++ x.c
@@ -679,6 +679,7 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+	clipcopy(NULL);
 }
 
 void
