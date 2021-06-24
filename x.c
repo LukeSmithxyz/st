44003a7cc6502886872eca9af415af1e67e3c204
Carlos Lobo <86011416+CarlosLoboxyz@users.noreply.github.com>
2021-06-24 10:44:20 -0400

    Apply bold-is-not-bright patch
    Patch name: 'st-bold-is-not-bright-20190127-3be4cf1.diff'

--- x.c
+++ x.c
@@ -1555,10 +1555,6 @@ xdrawglyphfontspecs(const XftGlyphFontSpec *specs, Glyph base, int len, int x, i
 		bg = &dc.col[base.bg];
 	}
 
-	/* Change basic system colors [0-7] to bright system colors [8-15] */
-	if ((base.mode & ATTR_BOLD_FAINT) == ATTR_BOLD && BETWEEN(base.fg, 0, 7))
-		fg = &dc.col[base.fg + 8];
-
 	if (IS_SET(MODE_REVERSE)) {
 		if (fg == &dc.col[defaultfg]) {
 			fg = &dc.col[defaultbg];
