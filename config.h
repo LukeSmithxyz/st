9dc4928e8ac3c767fe193c8ae3edde48c1574995
Mateusz Bagieński <mateuszb6a@gmail.com>
2023-02-11 23:26:37 +0100

    Patched with st-anysize

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
