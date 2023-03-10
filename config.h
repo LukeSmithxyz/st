e6ea2bf6da1bd1e28a44f00b6a1e8284e0f84009
Slin Lee <slin@slinlee.com>
2023-03-10 12:07:08 -0800

    Fix JetBrainsMono font name

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetbrainsMono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrainsMono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 2;
 
